9f4dde6a781de26957430dd8dd3683acb0bad54d
Rx Dev'iLL <hackdaemon@hackd.design>
2016-08-14 22:50:53 -0700

    License corrected

--- Frontend/src/app.component.ts
+++ Frontend/src/app.component.ts
@@ -22,7 +22,6 @@ import { FaComponent } from 'angular2-fontawesome/components';
             <br>
             <h4> Rex the i'll Dev </h4> <a href="mailto:rex@hackd.design"> email </a>
             <hr>
-            updated: 9-14-2016
             <h1>N A V</h1>
             <ul>
               <li>
